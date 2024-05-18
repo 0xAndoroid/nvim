27daa603f4e45aaa1a404b7dac956106374fab92
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2024-05-18 17:27:07 -0500

    fix: romevod notification on hover docs

--- lua/community.lua
+++ lua/community.lua
@@ -26,6 +26,15 @@ return {
           -- silent = true,
         },
       },
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
       presets = {
         lsp_doc_border = true,
       },
